17ecf9c69436bc189ffc33e9b8d9fe43e120b456
Suqian Wang <wangsuqian123@tamu.edu>
2019-02-13 15:30:54 -0600

    part 2 fix

--- ruby-on-rails/rottenpotatoes-rails-intro/app/models/movie.rb
+++ ruby-on-rails/rottenpotatoes-rails-intro/app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
 
 # grab a single record per unique value in the field 'rating'
   def self.get_ratings
-    self.pluck(:rating).distinct
+    self.pluck(:rating)
   end
 
 end
