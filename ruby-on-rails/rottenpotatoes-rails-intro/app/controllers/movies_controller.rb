ceaba6217b9ba2dae488e0697e8daee55ae7790f
Suqian Wang <wangsuqian123@tamu.edu>
2019-02-13 23:27:36 -0600

    part 3 complete

--- ruby-on-rails/rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
+++ ruby-on-rails/rottenpotatoes-rails-intro/app/controllers/movies_controller.rb
@@ -14,43 +14,53 @@ class MoviesController < ApplicationController
 
     @all_ratings = Movie.get_ratings
 
+    # if there is previous session saved but the URL lack of params, fill in the saved params
+    if (params[:ratings].nil? && !(session[:ratings].nil?)) || (params[:sort].nil? && !(session[:sort].nil?))
+      flash.keep
+      redirect_to movies_path(:ratings => session[:ratings], :sort => session[:sort])
+    end
+
     # if box(es) has been checked, params[:ratings] will not be empty, user choose filters
     if !(params[:ratings].nil?)
       # record the keys (the name of the boxes that has been checked) of the hash - ratings
-      @check_ratings = params[:ratings].keys
+      check_ratings = params[:ratings]
       # remember current session
-      session[:ratings] = @check_ratings
+      session[:ratings] = check_ratings
+
     # if user is not currently choosing filters, and there is previous session saved, use the memory
     # don't need to remember current session because current session didn't change
     elsif !(session[:ratings].nil?)
-      @check_ratings = session[:ratings]
+      check_ratings = session[:ratings]
+
     # initial
     elsif session[:ratings].nil?
-      @check_ratings = @all_ratings
+      check_ratings = Hash[@all_ratings.collect {|item| [item, 1]}]
       # remember current session
-      session[:ratings] = @check_ratings
+      session[:ratings] = check_ratings
     end
 
     # mark the checked box true
-    @check_ratings.each do |rating|
+    keys = session[:ratings].keys
+    keys.each do |rating|
       params[rating] = true
     end
 
-    @movies = Movie.where(:rating => @check_ratings)
+    @movies = Movie.where(:rating => check_ratings.keys)
 
     # if user is currently sorting, remember the sort field and current session
     if !(params[:sort].nil?)
-      @sort_field = params[:sort]
-      session[:sort] = @sort_field
+      sort_field = params[:sort]
+      session[:sort] = sort_field
+
     # if user is not sorting, apply previous sort session
     else
-      @sort_field = session[:sort]
+      sort_field = session[:sort]
     end
 
     # sort based on title or release_date
-    if @sort_field == "title"
+    if sort_field == "title"
       @movies = @movies.order(:title)
-    elsif @sort_field == "release_date"
+    elsif sort_field == "release_date"
       @movies = @movies.order(:release_date)
     end
 
